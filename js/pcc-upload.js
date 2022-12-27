b1d390616c787335fe966fa7b5722ce4cdc55ded
Thitipol P <p.thitipol@gmail.com>
2022-12-27 12:22:55 +0700

    Revert to watermark.js and copy code from img.ly tutorial

--- js/pcc-upload.js
+++ js/pcc-upload.js
@@ -1,58 +1,94 @@
-// load a url and a file object
-//var loadFile = function (event) {
-//var upload = document.querySelector('input[type=file]').files[0];
-//watermark([upload, './image/pcc-logo.png'])
-//  .image(watermark.image.lowerRight(0.5))
-//  .then(function (img) {
-//    document.getElementById('preview').appendChild(img);
-//  });};
+// hiding the div that will contain the images
+const imagesDiv = document.querySelector("#images");
+const fileInput = document.querySelector("#upload");
 
-const myWatermark = function () {
+fileInput.addEventListener("change", async (e) => {
+  const [file] = fileInput.files;
+  // displaying the uploaded image
+  const originalImage = document.querySelector("#originalImage");
+  originalImage.src = await fileToDataUri(file);
+  
+  // adding the image watermark to the original image
+  // and showing the watermarked image
+  const watermarkedImage = document.querySelector("#watermarkedImage");
+  const watermarkedImageWithText = document.querySelector(
+    "#watermarkedImageWithText"
+  );
+  
+  originalImage.addEventListener("load", async () => {
+    watermarkedImage.src = await watermarkImage(
+      originalImage,
+      "./image/pcc-logo.png"
+    );
+    
+    watermarkedImageWithText.src = watermarkImageWithText(
+      originalImage,
+      "IMG.LY"
+    );
+  });
+  
+  // making the div containing the image visible
+  imagesDiv.style.visibility = "visible";
+  
+  return false;
+});
 
-    wmark.init({
-      /* config goes here */
-      "position": "top-right", // default "bottom-right"
-      "opacity": 50, // default 50
-      "className": "preview-selected-image", // default "watermark"
-      "path": "./image/pcc-logo.png"
+function fileToDataUri(field) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      resolve(reader.result);
     });
+    
+    reader.readAsDataURL(field);
+  });
+}
 
-    load = true;
-  }
+async function watermarkImage(originalImage, watermarkImagePath) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  
+  const canvasWidth = originalImage.width;
+  const canvasHeight = originalImage.height;
+  
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  
+  // initializing the canvas with the original image
+  context.drawImage(originalImage, 0, 0, canvasWidth, canvasHeight);
+  
+  // loading the watermark image and transforming it into a pattern
+  const result = await fetch(watermarkImagePath);
+  const blob = await result.blob();
+  const image = await createImageBitmap(blob);
+  const pattern = context.createPattern(image, "no-repeat");
+  
+  // translating the watermark image to the bottom right corner
+  context.translate(canvasWidth - image.width, canvasHeight - image.height);
+  context.rect(0, 0, canvasWidth, canvasHeight);
+  context.fillStyle = pattern;
+  
+  return canvas.toDataURL();
+}
 
-
-/**
- * Create an arrow function that will be called when an image is selected.
- */
-const previewImage = (event) => {
-  /**
-   * Get the selected files.
-   */
-  const imageFiles = event.target.files;
-  /**
-   *
-   * Count the number of files selected.
-   */
-  const imageFilesLength = imageFiles.length;
-  /**
-   * If at least one image is selected, then proceed to display the preview.
-   */
-  if (imageFilesLength > 0) {
-      /**
-       * Get the image path.
-       */
-      const imageSrc = URL.createObjectURL(imageFiles[0]);
-      /**
-       * Select the image preview element.
-       */
-      const imagePreviewElement = document.querySelector("#preview-selected-image");
-      /**
-       * Assign the path to the image preview element.
-       */
-      imagePreviewElement.src = imageSrc;
-      /**
-       * Show the element by changing the display value to "block".
-       */
-      imagePreviewElement.style.display = "block";
-  }
-};
+function watermarkImageWithText(originalImage, watermarkText) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  
+  const canvasWidth = originalImage.width;
+  const canvasHeight = originalImage.height;
+  
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  
+  // initializing the canvas with the original image
+  context.drawImage(originalImage, 0, 0, canvasWidth, canvasHeight);
+  
+  // adding a blue watermark text in the bottom right corner
+  context.fillStyle = "blue";
+  context.textBaseline = "middle";
+  context.font = "bold 25px serif";
+  context.fillText(watermarkText, canvasWidth - 100, canvasHeight - 20);
+  
+  return canvas.toDataURL();
+}
